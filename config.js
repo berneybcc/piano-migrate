eff1833fa677f7d0e4705befadcd99e4af40b57b
berneybcc <57729351+berneybcc@users.noreply.github.com>
2020-08-04 10:55:39 -0500

    Se agrega documentacion

--- config.js
+++ config.js
@@ -1,7 +1,13 @@
+/**
+ * Configuracion de la base datos
+ */
 const HOST_DB ="127.0.0.1";
 const USER_DB ="root";
 const PASS_DB ="toor";
 const PORT_DB ="3306";
+/**
+ * Configuracion para SFTP
+ */
 const HOST_SFTP="127.0.0.1";
 const PORT_SFTP="22";
 const USER_SFTP="berneytest";
